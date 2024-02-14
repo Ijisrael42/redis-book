342d3a3b76047de7aa3f22b8727f4322cc2c61b5
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-02-14 16:18:35 +0200

    feat: Added Unique title for books

--- src/app/actions/create.js
+++ src/app/actions/create.js
@@ -9,6 +9,16 @@ export async function createBook(formData) {
   //create book id
   const id = Math.floor(Math.random() * 10000);
 
+  //add book title to the sorted set
+  const unique = await client.zAdd('books', {
+    value: title,
+    score: id,
+  }, { NX: true})
+
+  if (!unique) {
+    return {error: 'That has already been added'}
+  }
+
   //save new book
   await client.hSet(`books:${id}`, {
     title, rating, author, blurb
