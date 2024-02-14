274eeca2b28d8955bf35a6db5703c3f724f3d732
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2024-02-14 16:09:00 +0200

    Added Redis Client, Create Action

--- src/app/actions/create.js
+++ src/app/actions/create.js
@@ -1,10 +1,18 @@
 'use server'
 
-// import { client } from "@/lib/db"
+import { client } from "../lib/db"
 import { redirect } from 'next/navigation'
 
 export async function createBook(formData) {
   const {title, rating, author, blurb} = Object.fromEntries(formData)
 
-  
+  //create book id
+  const id = Math.floor(Math.random() * 10000);
+
+  //save new book
+  await client.hSet(`books:${id}`, {
+    title, rating, author, blurb
+  })
+
+  redirect('/')
 }
